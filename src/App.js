ab2ef6861e75c8b416e7b7c56cf3081e2397a89a
psarge2 <54369285+psarge2@users.noreply.github.com>
2021-01-13 23:59:59 -0500

    filtered input

--- src/App.js
+++ src/App.js
@@ -20,6 +20,10 @@ class App extends Component {
 
 
   render() {
+    const {monsters, searchField} = this.state;
+    const filteredMonsters = monsters.filter(monster =>
+      monster.name.toLowerCase().includes(searchField.toLowerCase())
+      );
     return (
       <div className="App">
         <input 
